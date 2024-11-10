66cda58d5b75a8e938ffac2f715659652d3fcc2f
shittu121 <shittufaruq6@gmail.com>
2024-11-10 06:39:35 +0000

    Update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,10 +11,10 @@ export function Card() {
       <GlareCard className="flex flex-col justify-center lg:px-8 lg:py-8 md:px-8 md:py-8 space-y-3 text-white">
         <div className="flex gap-2 mx-auto lg:mx-0 md:mx-0 flex-row items-center">
         <GoProjectSymlink className="h-6 w-6 text-white" />
-        <h1>Projects</h1>
+        <h1 className="font-bold text-sm lg:text-2xl md:text-xl">Projects</h1>
         </div>
-        <div className="flex items-center mx-auto lg:mx-0 md:mx-0">
-        <h1 className="font-bold text-2xl">2040</h1>
+        <div className="flex items-center mx-2 lg:mx-0 md:mx-0">
+        <h1 className="font-bold text-sm lg:text-2xl md:text-xl">2040</h1>
         <ChartDashboard />
 
         </div>
@@ -24,10 +24,10 @@ export function Card() {
       <GlareCard className="flex flex-col justify-center lg:px-8 lg:py-8 md:px-8 md:py-8 space-y-3 text-white">
       <div className="flex gap-2 mx-auto lg:mx-0 md:mx-0 flex-row items-center">
         <FaRegUser className="w-6 h-6 text-white" />
-        <h1>Clients</h1>
+        <h1 className="font-bold text-sm lg:text-2xl md:text-xl">Clients</h1>
         </div>
-        <div className="flex items-center mx-auto lg:mx-0 md:mx-0">
-        <h1 className="font-bold text-2xl">158</h1>
+        <div className="flex items-center mx-2 lg:mx-0 md:mx-0">
+        <h1 className="font-bold text-sm lg:text-2xl md:text-xl">158</h1>
         <ChartDashboard lineColor="rgb(255, 99, 132)" />
 
         </div>
@@ -35,10 +35,10 @@ export function Card() {
       <GlareCard className="flex flex-col justify-center lg:px-8 lg:py-8 md:px-8 md:py-8 space-y-3 text-white">
       <div className="flex gap-2 mx-auto lg:mx-0 md:mx-0 flex-row items-center">
         <FaSackDollar className="w-6 h-6 text-white" />
-        <h1>Revenue</h1>
+        <h1 className="font-bold text-sm lg:text-2xl md:text-xl">Revenue</h1>
         </div>
-        <div className="flex items-center mx-auto lg:mx-0 md:mx-0">
-        <h1 className="font-bold text-2xl">$75K</h1>
+        <div className="flex items-center mx-2 lg:mx-0 md:mx-0">
+        <h1 className="font-bold text-sm lg:text-2xl md:text-xl">$75K</h1>
         <ChartDashboard lineColor="#4caf50" />
 
         </div>
